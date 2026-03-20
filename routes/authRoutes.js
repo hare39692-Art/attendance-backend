b51388a65971961d221d1d1e8c65e6fd21ef0110
Hare <hare39692@gmail.com>
2026-03-20 15:42:01 +0530

    fcm token added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 const { login, createAdmin, changePassword, getProfile } = require('../controllers/authController');
+const { saveFcmToken } = require('../controllers/notificationController');
 const protect = require('../middleware/authMiddleware');
 
 router.post('/login',           login);
 router.post('/create-admin',    protect(['admin']), createAdmin);
 router.put('/change-password',  protect(['admin', 'teacher', 'student']), changePassword);
 router.get('/profile',          protect(['admin', 'teacher', 'student']), getProfile);
+router.post('/save-fcm-token',  protect(['admin', 'teacher', 'student']), saveFcmToken);
 
 module.exports = router;
